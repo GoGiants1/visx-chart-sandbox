7b69574d19b63b6b1a7cc2a89bf748021070cec7
HyungWook <69342392+GoGiants1@users.noreply.github.com>
2022-06-24 15:19:28 +0900

    add brush chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import BrushChart from "./charts/BrushChart";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <BrushChart width={600} height={500} />
     </div>
   );
 }
